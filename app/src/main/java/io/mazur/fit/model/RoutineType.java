8679525f98b21c334a9937159b050d9c1e299b6d
Benoit Dion <benoitdion@gmail.com>
2015-06-30 14:31:34 -0400

    Use enum ordinal instead of mirrored constants

--- app/src/main/java/io/mazur/fit/model/RoutineType.java
+++ app/src/main/java/io/mazur/fit/model/RoutineType.java
@@ -3,5 +3,11 @@ package io.mazur.fit.model;
 public enum RoutineType {
     CATEGORY,
     SECTION,
-    EXERCISE
+    EXERCISE;
+
+    private static RoutineType[] values = RoutineType.values();
+
+    public static RoutineType fromInt(int value) {
+        return values[value];
+    }
 }
