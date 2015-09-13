43de29b1c39ecf97a18d379ff1b2a9e966021db8
Damian Mazurkiewicz <damian@mazur.io>
2015-09-13 17:50:56 +0100

    Changing default tag in logger.

--- app/src/main/java/io/mazur/fit/utils/Logger.java
+++ app/src/main/java/io/mazur/fit/utils/Logger.java
@@ -3,8 +3,6 @@ package io.mazur.fit.utils;
 import android.util.Log;
 
 public class Logger {
-    public static void e(String msg) { Log.e("Fit 3s", msg); }
-    public static void d(String msg) {
-        Log.d("Fit 3s", msg);
-    }
+    public static void e(String msg) { Log.e("Bodyweight Fitness", msg); }
+    public static void d(String msg) { Log.d("Bodyweight Fitness", msg); }
 }
